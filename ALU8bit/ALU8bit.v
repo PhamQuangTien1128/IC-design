6aed70dc05fb52b0f37ad4f78ecc0fcf39db2ef4
Tien Pham <phamquangtien1128@gmail.com>
2024-07-23 23:57:35 +0700

    Add files via upload

--- ALU8bit/ALU8bit.v
+++ ALU8bit/ALU8bit.v
@@ -20,8 +20,8 @@ wire CarryOut;
 assign less[7:1] = 7'b0;
 assign less[0] = result[7];
 
-assign Binv = (Op == 3'd10) ? 1'b1 : 1'b0;
-assign c0 = (Op == 3'd10) ? 1'b1 : 1'b0;
+assign Binv = (Op == 4'b1010) ? 1'b1 : 1'b0;
+assign c0 = (Op == 4'b1010) ? 1'b1 : 1'b0;
 
 //Inverter
 assign invResult = ~a;
@@ -33,61 +33,74 @@ assign andResult = a & b;
 assign orResult = a | b;
 
 //Arithmetic
-adder8bit (.a(a), .b(b), .c0(c0), .Binv(Binv), .Sum(arithmetic), .Carry(CarryOut));
+adder8bit A80(.a(a), .b(b), .c0(c0), .Binv(Binv), .Sum(arithmetic), .Carry(CarryOut));
 
 //Multiply
-Multiply(.a_in(a), .b_in(b), .product(netProduct));
+Multiply M0(.a_in(a), .b_in(b), .product(netProduct));
 
 //Overflow
-Overflow (.Binv(Binv), .c0(c0), .a(a[7]), .b(b[7]), .CarryOut(CarryOut), .r(arithmetic[7]), .OF(OFnet));
+Overflow OV(.Binv(Binv), .c0(c0), .a(a[7]), .b(b[7]), .CarryOut(CarryOut), .r(arithmetic[7]), .OF(OFnet));
 
 
 always @(a, b, Op) begin
 	case(Op)
-		4'd0:begin //inverter
+		4'b0000:begin //inverter
 			result <= invResult;
+			product <= 16'b0;
 		end
-		4'd1:begin //AND bitwise
+		4'b0001:begin //AND bitwise
 			result <= andResult;
+			product <= 16'b0;
 		end
-		4'd2:begin //OR bitwise
+		4'b0010:begin //OR bitwise
 			result <= orResult;
+			product <= 16'b0;
 		end
-		4'd3:begin //shift right logic 1 bit
+		4'b0011:begin //shift right logic 1 bit
 			result <= {1'b0, a[7:1]};
+			product <= 16'b0;
 		end
-		4'd4:begin //shift left logic 1 bit
+		4'b0100:begin //shift left logic 1 bit
 			result <= {a[6:0], 1'b0};
+			product <= 16'b0;
 		end
-		4'd5:begin //shift right arithmetic 1 bit
+		4'b0101:begin //shift right arithmetic 1 bit
 			case(a[7])
 				1'b1: result <= {1'b1, a[7:1]};
 				1'b0: result <= {1'b0, a[7:1]};
 			endcase
+			product <= 16'b0;
 		end
-		4'd6:begin //shift left arithmetic 1 bit
+		4'b0110:begin //shift left arithmetic 1 bit
 			case(a[7])
 				1'b1: result <= {1'b1, a[5:0], 1'b0};
 				1'b0: result <= {a[6:0], 1'b0};
 			endcase
+			product <= 16'b0;
 		end
-		4'd7:begin //Rotate right
+		4'b0111:begin //Rotate right
 			result <= {a[0], a[7:1]};
+			product <= 16'b0;
 		end
-		4'd8:begin //Rotate left
+		4'b1000:begin //Rotate left
 			result <= {a[6:0], a[7]};
+			product <= 16'b0;
 		end
-		4'd9:begin //add
+		4'b1001:begin //add
 			result <= arithmetic;
+			product <= 16'b0;
 		end
-		4'd10:begin //sub
+		4'b1010:begin //sub
 			result <= arithmetic;
+			product <= 16'b0;
 		end
-		4'd11:begin
+		4'b1011:begin //multiply
 			product <= netProduct;
+			result <= 8'b0;
 		end
 		default:begin //set zero
 			result <= 8'b0;
+			product <= 16'b0;
 		end
 	endcase
 	OF <= OFnet;
@@ -103,10 +116,10 @@ wire [1:0] G;
 wire Carry1;
 wire Carry2;
 
-adder4bit A0(.a(a[3:0]), .b(b[3:0]), .CI(c0), .Binv(Binv), .result(Sum[3:0]), .P_out(P[0]), .G_out(G[0]));
-CarryIn1 U0(.g0(G[0]), .p0(P[0]), .c0(c0), .c1(Carry1));
-adder4bit A1(.a(a[7:4]), .b(b[7:4]), .CI(Carry1), .Binv(Binv), .result(Sum[7:4]), .P_out(P[1]), .G_out(G[1]));
-CarryIn2 U1(.g(G[1:0]), .p(P[1:0]), .c0(c0), .c2(Carry2));
+adder4bit A40(.a(a[3:0]), .b(b[3:0]), .CI(c0), .Binv(Binv), .result(Sum[3:0]), .P_out(P[0]), .G_out(G[0]));
+CarryIn1 C1(.g0(G[0]), .p0(P[0]), .c0(c0), .c1(Carry1));
+adder4bit A41(.a(a[7:4]), .b(b[7:4]), .CI(Carry1), .Binv(Binv), .result(Sum[7:4]), .P_out(P[1]), .G_out(G[1]));
+CarryIn2 C2(.g(G[1:0]), .p(P[1:0]), .c0(c0), .c2(Carry2));
 assign Carry = Carry2;
 
 endmodule
@@ -123,27 +136,27 @@ wire netP, netG;
 
 assign bInv = (Binv) ? ~b : b;
 
-_generate U0(.ai(a[0]), .bi(bInv[0]), .gi(net_g[0]));
-_generate U1(.ai(a[1]), .bi(bInv[1]), .gi(net_g[1]));
-_generate U2(.ai(a[2]), .bi(bInv[2]), .gi(net_g[2]));
-_generate U3(.ai(a[3]), .bi(bInv[3]), .gi(net_g[3]));
+_generate _G0(.ai(a[0]), .bi(bInv[0]), .gi(net_g[0]));
+_generate _G1(.ai(a[1]), .bi(bInv[1]), .gi(net_g[1]));
+_generate _G2(.ai(a[2]), .bi(bInv[2]), .gi(net_g[2]));
+_generate _G3(.ai(a[3]), .bi(bInv[3]), .gi(net_g[3]));
 
-_propagate M0(.ai(a[0]), .bi(bInv[0]), .pi(net_p[0]));
-_propagate M1(.ai(a[1]), .bi(bInv[1]), .pi(net_p[1]));
-_propagate M2(.ai(a[2]), .bi(bInv[2]), .pi(net_p[2]));
-_propagate M3(.ai(a[3]), .bi(bInv[3]), .pi(net_p[3]));
+_propagate _P0(.ai(a[0]), .bi(bInv[0]), .pi(net_p[0]));
+_propagate _P1(.ai(a[1]), .bi(bInv[1]), .pi(net_p[1]));
+_propagate _P2(.ai(a[2]), .bi(bInv[2]), .pi(net_p[2]));
+_propagate _P3(.ai(a[3]), .bi(bInv[3]), .pi(net_p[3]));
 
-CarryIn1 Q1(.g0(net_g[0]), .p0(net_p[0]), .c0(c0), .c1(c1));
-CarryIn2 Q2(.g(net_g[1:0]), .p(net_p[1:0]), .c0(c0), .c2(c2));
-CarryIn3 Q3(.g(net_g[2:0]), .p(net_p[2:0]), .c0(c0), .c3(c3));
+CarryIn1 _CI1(.g0(net_g[0]), .p0(net_p[0]), .c0(CI), .c1(c1));
+CarryIn2 _CI2(.g(net_g[1:0]), .p(net_p[1:0]), .c0(CI), .c2(c2));
+CarryIn3 _CI3(.g(net_g[2:0]), .p(net_p[2:0]), .c0(CI), .c3(c3));
 
-Propagate P(.p(net_p[3:0]), .P(netP));
-Generate G(.g(net_g[3:0]), .p(net_p[3:0]), .G(netG));
+Propagate P0(.p(net_p[3:0]), .P(netP));
+Generate G0(.g(net_g[3:0]), .p(net_p[3:0]), .G(netG));
 
-adder1bit A0(.a(a[0]), .b(bInv[0]), .ci(c0), .result(net_result[0]));
-adder1bit A1(.a(a[1]), .b(bInv[1]), .ci(c1), .result(net_result[1]));
-adder1bit A2(.a(a[2]), .b(bInv[2]), .ci(c2), .result(net_result[2]));
-adder1bit A3(.a(a[3]), .b(bInv[3]), .ci(c3), .result(net_result[3]));
+adder1bit A10(.a(a[0]), .b(bInv[0]), .ci(CI), .result(net_result[0]));
+adder1bit A11(.a(a[1]), .b(bInv[1]), .ci(c1), .result(net_result[1]));
+adder1bit A12(.a(a[2]), .b(bInv[2]), .ci(c2), .result(net_result[2]));
+adder1bit A13(.a(a[3]), .b(bInv[3]), .ci(c3), .result(net_result[3]));
 
 assign result = net_result; assign P_out = netP; assign G_out = netG;
 endmodule
@@ -197,8 +210,9 @@ wire [7:0] a, b;
 assign a_2 = (a_in[7]) ? (~a_in) : a_in;
 assign b_2 = (b_in[7]) ? (~b_in) : b_in;
 
-adder8bit M0(.a(a_2), .b(1'b1), .c0(1'b0), .Binv(1'b0), .Sum(a_22), .Carry(GND));
-adder8bit M1(.a(b_2), .b(1'b1), .c0(1'b0), .Binv(1'b0), .Sum(b_22), .Carry(GND));
+//Two's Complement of inputs
+adder8bit IN_TC0(.a(a_2), .b(1'b1), .c0(1'b0), .Binv(1'b0), .Sum(a_22), .Carry());
+adder8bit IN_TC1(.a(b_2), .b(1'b1), .c0(1'b0), .Binv(1'b0), .Sum(b_22), .Carry());
 
 assign a = (a_in[7]) ? (a_22) : a_in;
 assign b = (b_in[7]) ? (b_22) : b_in;
@@ -371,19 +385,19 @@ adder8bit ADD1(.a(net20), .b(net30), .c0(1'b0), .Binv(1'b0), .Sum(add1Result), .
 adder8bit ADD2(.a(net40), .b(net50), .c0(1'b0), .Binv(1'b0), .Sum(add2Result), .Carry(carry2));
 adder8bit ADD3(.a(net60), .b(net70), .c0(1'b0), .Binv(1'b0), .Sum(add3Result), .Carry(carry3));
 
-adder8bit ADD4(.a(net01), .b(net11), .c0(carry0), .Binv(1'b0), .Sum(add4Result), .Carry(GND));
-adder8bit ADD5(.a(net21), .b(net31), .c0(carry1), .Binv(1'b0), .Sum(add5Result), .Carry(GND));
-adder8bit ADD6(.a(net41), .b(net51), .c0(carry2), .Binv(1'b0), .Sum(add6Result), .Carry(GND));
-adder8bit ADD7(.a(net61), .b(net71), .c0(carry3), .Binv(1'b0), .Sum(add7Result), .Carry(GND));
+adder8bit ADD4(.a(net01), .b(net11), .c0(carry0), .Binv(1'b0), .Sum(add4Result), .Carry());
+adder8bit ADD5(.a(net21), .b(net31), .c0(carry1), .Binv(1'b0), .Sum(add5Result), .Carry());
+adder8bit ADD6(.a(net41), .b(net51), .c0(carry2), .Binv(1'b0), .Sum(add6Result), .Carry());
+adder8bit ADD7(.a(net61), .b(net71), .c0(carry3), .Binv(1'b0), .Sum(add7Result), .Carry());
 
 adder8bit ADD8(.a(add0Result), .b(add1Result), .c0(1'b0), .Binv(1'b0), .Sum(add8Result), .Carry(carry4));
 adder8bit ADD9(.a(add2Result), .b(add3Result), .c0(1'b0), .Binv(1'b0), .Sum(add9Result), .Carry(carry5));
 
-adder8bit ADD10(.a(add4Result), .b(add5Result), .c0(carry4), .Binv(1'b0), .Sum(add10Result), .Carry(GND));
-adder8bit ADD11(.a(add6Result), .b(add7Result), .c0(carry5), .Binv(1'b0), .Sum(add11Result), .Carry(GND));
+adder8bit ADD10(.a(add4Result), .b(add5Result), .c0(carry4), .Binv(1'b0), .Sum(add10Result), .Carry());
+adder8bit ADD11(.a(add6Result), .b(add7Result), .c0(carry5), .Binv(1'b0), .Sum(add11Result), .Carry());
 
 adder8bit ADD12(.a(add8Result), .b(add9Result), .c0(1'b0), .Binv(1'b0), .Sum(add12Result), .Carry(carry6));
-adder8bit ADD13(.a(add10Result), .b(add11Result), .c0(carry6), .Binv(1'b0), .Sum(add13Result), .Carry(GND));
+adder8bit ADD13(.a(add10Result), .b(add11Result), .c0(carry6), .Binv(1'b0), .Sum(add13Result), .Carry());
 
 wire [7:0] add12Result_2, add12Result_22;
 wire [7:0] add13Result_2, add13Result_22;
@@ -392,8 +406,9 @@ wire carry7;
 assign add12Result_2 = (check) ? (~add12Result) : add12Result;
 assign add13Result_2 = (check) ? (~add13Result) : add13Result;
 
-adder8bit TC0(.a(add12Result_2), .b(1'b1), .c0(1'b0), .Binv(1'b0), .Sum(add12Result_22), .Carry(carry7));
-adder8bit TC1(.a(add13Result_2), .b(1'b0), .c0(carry7), .Binv(1'b0), .Sum(add13Result_22), .Carry(GND));
+//Two's Complement of product
+adder8bit OUT_TC0(.a(add12Result_2), .b(1'b1), .c0(1'b0), .Binv(1'b0), .Sum(add12Result_22), .Carry(carry7));
+adder8bit OUT_TC1(.a(add13Result_2), .b(1'b0), .c0(carry7), .Binv(1'b0), .Sum(add13Result_22), .Carry());
 
 assign product[7:0] = (check) ? add12Result_22 : add12Result;
 assign product[15:8] = (check) ? add13Result_22 : add13Result;
@@ -409,3 +424,6 @@ endmodule
 module Overflow(input Binv, input c0, input a, input b, input CarryOut, input r, output OF);
 	assign OF = (~c0 & ~Binv & ((~a & ~b & r) | (~a & ~b & CarryOut) | (a & b & ~CarryOut & ~r))) | (c0 & Binv & ((a & ~b & ~CarryOut & ~r) | (~a & b & r) | (~a & b & CarryOut)));
 endmodule
+
+
+
