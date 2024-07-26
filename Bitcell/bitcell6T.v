f950ea1fa64f0f76038c4e5178e34665399ff485
Tien Pham <phamquangtien1128@gmail.com>
2024-07-27 00:34:58 +0700

    Add files via upload

--- Bitcell/bitcell6T.v
+++ Bitcell/bitcell6T.v
@@ -1,18 +1,8 @@
-module bitcell6T(input D_in, input En, output D_out_bar);
-wire C1, C2;
-assign C1 = (En) ? D_in : 1'bz;
-assign D_out_bar = (En) ? C2 : 1'bz;
-memcell U0(.C1(C1), .C2(C2));
-endmodule
-
-module memcell(inout C1, inout C2);
+module bitcell6T(input D, input En, output D_out);
 wire a, b, c, d;
-
+assign a = (En) ? D : d;
 assign b = ~a;
-assign c = b;
+assign c = (En) ? (~D) : b;
 assign d = ~c;
-assign a = d;
-
-assign a = C1;
-assign C2 = b;
+assign D_out = b;
 endmodule
--- Bitcell/bitcell8T.v
+++ Bitcell/bitcell8T.v
@@ -1,24 +1,9 @@
-module bitcell8T(input WriteEn, input ReadEn, inout D, inout D_bar);
-wire [1:0] D1;
-wire [1:0] D2;
-
-memcell U0(.D1(D1[0]), .D2(D1[1]));
-memcell U1(.D1(D2[0]), .D2(D2[1]));
-
-assign D1[0] = (ReadEn) ? D : 1'bz;
-assign D = (WriteEn) ? D2[0] : 1'bz;
-assign D2[1] = (ReadEn) ? D_bar : 1'bz;
-assign D_bar = (WriteEn) ? D1[1] : 1'bz;
-endmodule
-
-module memcell(inout D1, inout D2);
+//WE: Write Enable; RE: Read Enable; RBL: Read Bit Line
+module bitcell8T(input WE, input RE, input D, output RBL);
 wire a, b, c, d;
-
+assign a = (WE) ? D : d;
 assign b = ~a;
-assign c = b;
+assign c = (WE) ? (~D) : b;
 assign d = ~c;
-assign a = d;
-
-assign a = D1;
-assign D2 = b;
+assign RBL = (~RE) ? 1'bz : (b) ? 1'b0 : 1'b1;
 endmodule
