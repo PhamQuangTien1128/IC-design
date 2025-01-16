9a0ff281d01825f42af67a36aae4ea0e1d0756e8
Tien Pham <phamquangtien1128@gmail.com>
2025-01-16 16:39:40 +0700

    Delete Bitcell directory

--- Bitcell/bitcell6T.v
+++
@@ -1,8 +0,0 @@
-module bitcell6T(input D, input En, output D_out);
-wire a, b, c, d;
-assign a = (En) ? D : d;
-assign b = ~a;
-assign c = (En) ? (~D) : b;
-assign d = ~c;
-assign D_out = b;
-endmodule
--- Bitcell/bitcell8T.v
+++
@@ -1,9 +0,0 @@
-//WE: Write Enable; RE: Read Enable; RBL: Read Bit Line
-module bitcell8T(input WE, input RE, input D, output RBL);
-wire a, b, c, d;
-assign a = (WE) ? D : d;
-assign b = ~a;
-assign c = (WE) ? (~D) : b;
-assign d = ~c;
-assign RBL = (RE) ? (~RE & b) : 1'bz;
-endmodule
